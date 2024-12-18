5b5cb36ac6294aacebcc391524a22912e8b63e8a
ArAhmadRaza <arahmadraza5570@gmail.com>
2024-12-18 15:52:19 +0500

    interface

---
+++ Practices/Practice no 88/script.js
@@ -0,0 +1,28 @@
+"use strict";
+// interface wer{
+//     name : string;
+//     age : number;
+//     city : string;
+// }
+// interface rew extends wer {
+//     height : number;
+// }
+// var poi:rew = {
+//     name : "admin",
+//     age : 30,
+//     city : "shanghai",
+//     height : 180,
+// }
+// var obj:wer = {
+//     name : "loser",
+//     age : 20,
+//     city : "beijing",
+// }
+// console.log(poi);
+//Union
+// var wer:number | string = "rew"
+// console.log(wer);
+function wer(a, b) {
+    return a + b;
+}
+console.log(wer(10, 20));
---
+++ Practices/Practice no 88/script.ts
@@ -0,0 +1,54 @@
+
+// interface wer{
+//     name : string;
+//     age : number;
+//     city : string;
+// }
+
+// interface rew extends wer {
+//     height : number;
+// }
+
+// var poi:rew = {
+//     name : "admin",
+//     age : 30,
+//     city : "shanghai",
+//     height : 180,
+// }
+// var obj:wer = {
+//     name : "loser",
+//     age : 20,
+//     city : "beijing",
+// }
+
+
+// console.log(poi);
+
+
+
+
+
+
+
+//Union
+
+// var wer:number | string = "rew"
+
+// console.log(wer);
+
+
+// function wer(a:number,b:number) {
+//     return a + b
+
+// console.log(wer(10,20));
+
+
+
+
+
+
+
+
+
+
+
