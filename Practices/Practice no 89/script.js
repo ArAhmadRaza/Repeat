d507e7fa05c7721e9d76cc11aef44f80a3cb4b17
ArAhmadRaza <arahmadraza5570@gmail.com>
2024-12-18 17:33:32 +0500

    Objects through class in TS

---
+++ Practices/Practice no 89/script.js
@@ -0,0 +1,9 @@
+var Student = /** @class */ (function () {
+    function Student(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return Student;
+}());
+var student = new Student("John", 20);
+console.log(student);
---
+++ Practices/Practice no 89/script.ts
@@ -0,0 +1,24 @@
+
+
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+
+// class Student implements Person {
+//     name: string;
+//     age: number;
+//     constructor(name: string, age: number) {
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+
+// var student = new Student("John", 20);
+// console.log(student);
+
+
+
+
+
+
