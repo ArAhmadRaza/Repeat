8e198f6635692fafd77c42e1bf91dc5f22fa6ac8
ArAhmadRaza <arahmadraza5570@gmail.com>
2024-12-25 15:49:40 +0500

    ts

---
+++ Practices/Practice no 94/script.ts
@@ -0,0 +1,101 @@
+
+// // abstraction
+
+// interface ILaptop {
+//     name : string;  
+//     price : number;
+//     login() : void;
+// }
+
+// abstract class Laptop {
+//     name : string;
+//     price : number;
+//     constructor(name : string, price : number){
+//         this.name = name;
+//         this.price = price;
+        
+//     } 
+
+// }
+
+// interface IAdmin extends ILaptop{
+//     wer : string
+// }
+
+
+// class Admin extends Laptop  implements IAdmin{
+//     wer : string  
+//     constructor(name : string, price : number,wer : string){
+//         super(name,price)
+//         this.wer = wer
+//     }
+//     login(): void {
+//         console.log('hcgmjm,');
+        
+//     }
+// }
+
+//Encapsulation
+
+// interface ILaptop {
+//     name : string;  
+//     price : number;
+//     id: number;
+// }
+
+// class Laptop implements ILaptop{
+//    private name : string;
+//    public price : number;
+//    protected id : number;
+//     constructor(name : string, price : number, id : number){
+//         // this.name = name;
+//         this.price = price;
+//         this.id = id      
+//     } 
+
+//     getName(){
+
+//     }
+// }
+
+
+
+
+// Inheritance
+// extend the class with the previous class
+
+//Mulitlevel Inheritence
+// class A {
+
+// }
+// class B extends A {
+
+// }
+
+// class C extends B {
+
+// }
+
+// Muliple Inheritence
+// class A {
+
+// }
+// class B extends A {
+
+// }
+
+// class C extends A {
+
+// }
+
+
+
+
+//Polymorphism
+
+
+
+
+
+
+
