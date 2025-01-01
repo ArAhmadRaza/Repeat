23b83024bdfc14632837478276d57d8c8a707195
ArAhmadRaza <arahmadraza5570@gmail.com>
2025-01-01 17:35:01 +0500

    props practice

--- Practices/Reactjs/my-app/src/App.js
+++ Practices/Reactjs/my-app/src/App.js
@@ -1,14 +1,16 @@
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
 import Button from "./components/button/Button"
+import { Button2 } from "./components/button/Button"
 
 function App(){
   return (
     <div>
         <Header />
-      <h1>Hello Duffer</h1>
-      <Button />
+      <h1>Hello World 1</h1>
+      <Button title="login" color="blue" />
       <Footer />
+      <Button2 title="duffer"  color="yellow"/>
     </div>
   )
 }
--- Practices/Reactjs/my-app/src/components/button/Button.js
+++ Practices/Reactjs/my-app/src/components/button/Button.js
@@ -2,25 +2,21 @@
 import "./button.css"
 
 
-function Button(){
+ export default function Button(props){
+
     return(
         <div>
-            <button onClick={() => alert('Hello, world!')}>Click Me</button>
-            <br/>
-            <button onClick={() => alert('Goodbye, world!')}>Goodbye</button>
-            <br/>
-            <button onClick={() => alert('Hello, world! Twice')}>Click Me Twice</button>
-            <br/>
-            <button onClick={() => alert('Goodbye, world! Twice')}>Goodbye Twice</button>
-            <br/>
-            <button onClick={() => alert('Hello, world! Thrice')}>Click Me Thrice</button>
-            <br/>
-            <button onClick={() => alert('Goodbye, world! Thrice')}>Goodbye Thrice</button>
-            <br/>
-            <button onClick={() => alert('Hello, world! Four times')}>Click Me Four Times</button>
-            <br/>
+            <button ClassName="button" style={{backgroundColor: props.color}} >{props.title}</button>
         </div>
     )
 }
 
-export default Button
+export  function Button2(props){
+    return(
+        <div>
+            <button ClassName="button" style={{backgroundColor: props.color}} >{props.title}</button>
+        </div>
+    )
+}
+
+
